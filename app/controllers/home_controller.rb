4fb3cfa2feb5b8513de092976173a057c6ab0bcf
jamesjn <james.jn.chiang@gmail.com>
2012-08-10 18:54:34 -0400

    add buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < UIViewController
-  
+  attr_accessor :image  
+
   def init
     self
   end
@@ -8,7 +9,33 @@ class HomeController < UIViewController
   end
 
   def viewDidLoad
-    self.view.backgroundColor = UIColor.blueColor
+    view.backgroundColor = UIColor.alloc.initWithPatternImage(UIImage.imageNamed("brushedmetal.png"))
+    view.addSubview(image_view) 
+    view.addSubview(get_image_button) 
+    view.addSubview(set_text_button) 
+  end
+
+  private
+
+  def image_view
+    view = UIImageView.alloc.init
+    view.frame = [[10,10],[300,380]]
+    view.backgroundColor = UIColor.redColor
+    view
+  end
+
+  def get_image_button 
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.frame = [[20,400],[100,40]]
+    button.setTitle('Get Image', forState:UIControlStateNormal)
+    button
+  end
+
+  def set_text_button 
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.frame = [[140,400],[100,40]]
+    button.setTitle('Set Text', forState:UIControlStateNormal)
+    button
   end
 
 end
--- spec/home_controller_spec.rb
+++ spec/home_controller_spec.rb
@@ -4,8 +4,41 @@ describe "HomeController" do
     @subject = @app.windows.first.rootViewController
   end
 
-  it "has a blue background" do
-    @subject.view.backgroundColor.should == UIColor.blueColor
+  it "has background pattern " do
+    #TO DO figure out how to check for background patterns
+    @subject.view.backgroundColor.class.should == UICGColor 
   end
+
+  it "has an image of nil initially" do
+    @subject.image.should == nil
+  end 
+
+  it "image_view returns UIImageView" do
+    image_view = @subject.send(:image_view)
+    image_view.class.should == UIImageView
+  end 
+
+  it "has buttons that returns UIRoundedRectButton" do
+    button = @subject.send(:get_image_button)
+    button.class.should == UIRoundedRectButton 
+    button = @subject.send(:set_text_button)
+    button.class.should == UIRoundedRectButton 
+  end 
+
+  it "has a UIImageView subview with no initial image" do
+    subview = @subject.view.subviews.first
+    subview.class.should == UIImageView
+    subview.image.should == nil
+  end 
+  
+  it "has button subviews" do
+    subviews = @subject.view.subviews
+    button_subviews = subviews.select{|s| s.class == UIRoundedRectButton}
+    button_subviews.size.should == 2
+    ['Get Image', 'Set Text'].each do |title|
+      label_titles = button_subviews.map(&:titleLabel).map(&:text)
+      label_titles.include?(title).should.equal(true)
+    end
+  end 
 
 end
